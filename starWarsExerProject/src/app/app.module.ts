289941b2c8ef1ff4b1202602252fef065c87f999
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-10-18 09:44:39 +0200

    InterfacesDone. MaterialImportsDone

--- starWarsExerProject/src/app/app.module.ts
+++ starWarsExerProject/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CharactersComponent } from './components/characters/characters.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { CharactersDialogComponent } from './components/characters/characters-dialog/characters-dialog.component';
 import { SpeciesComponent } from './components/species/species.component';
 import { SpeciesDialogComponent } from './components/species/species-dialog/species-dialog.component';
+import { ImportsMaterialModule } from './imports-material/imports-material.module';
 
 
 @NgModule({
@@ -22,10 +19,8 @@ import { SpeciesDialogComponent } from './components/species/species-dialog/spec
     SpeciesDialogComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    NgbModule,
-    BrowserAnimationsModule
+    ImportsMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
