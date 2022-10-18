ad6c0c573c20963703fb402d457597f5842bf614
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-10-18 09:24:56 +0200

    characters&species enviorments created

--- starWarsExerProject/src/app/app.module.ts
+++ starWarsExerProject/src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CharactersComponent } from './components/characters/characters.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CharactersDialogComponent } from './components/characters/characters-dialog/characters-dialog.component';
+import { SpeciesComponent } from './components/species/species.component';
+import { SpeciesDialogComponent } from './components/species/species-dialog/species-dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    PageNotFoundComponent,
+    CharactersDialogComponent,
+    SpeciesComponent,
+    SpeciesDialogComponent,
   ],
   imports: [
     BrowserModule,
